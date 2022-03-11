e14962cb838520f3c27a0eff73ea10b31f140a99
fanhuirong <fanhuirong321@gmail.com>
2022-03-11 21:39:55 +0800

    feat: init docs

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production'
 const nextConfig = {
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
   reactStrictMode: true,
+  PUBLIC_URL: "https://fanhuirong.github.io/InternetBigNews/",
+  assetPrefix: './'
 }
-
 module.exports = nextConfig
